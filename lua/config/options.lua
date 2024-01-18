5c3989ac360f865ab9b252bfb5dc0cc6859b88cd
abhisarshukla <abhisar.shukla@bajajfinserv.in>
2024-01-18 12:25:43 +0530

    Add ts and json LSP, add bash.exe shell for windows

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+if vim.fn.has("windows") then
+  vim.opt.shell = "bash.exe"
+end
+
+if vim.g.neovide then
+  vim.g.neovide_fullscreen = true
+  vim.g.neovide_remember_window_size = true
+end
